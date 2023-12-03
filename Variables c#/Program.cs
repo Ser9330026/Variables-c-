adb41976f559759381cda550f4510f8dd6b3ac08
ser9330026 <ser9330026@yandex.ru>
2023-12-03 23:05:30 +0500

    Добавьте файлы проекта.

---
+++ Variables c#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variables_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 555;
+            string str = "Мама мыла раму";
+            Console.WriteLine(a);
+            Console.WriteLine(str);
+            bool var = true, var1 = false;
+            bool Var2 = false;
+            Console.WriteLine(var);
+            Console.WriteLine(var1);
+            Console.WriteLine(Var2);
+            double c = 3.6;
+            Console.WriteLine(c);
+            char ch = 'a';
+            Console.WriteLine(ch);
+            int dd = 9;
+            Console.WriteLine(dd);
+            dd = 999;
+            Console.WriteLine(dd);           
+        }
+    }
+}
